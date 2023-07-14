eafc16466d9ab5bb7fbfc803401ee55ebb79feaf
Oleg Usenko <oleg911usenko@gmail.com>
2023-07-14 12:51:01 +0300

    module 6.3 complete + add translate in module 6.1, 6.2

---
+++ school_lesson_6_3/models/library_author.py
@@ -0,0 +1,21 @@
+from odoo import fields, models, _
+
+
+class LibraryAuthor(models.Model):
+    _inherit = 'library.author'
+
+    biography = fields.Text(translate=True)
+
+    first_name = fields.Char(required=True, translate=True)
+    last_name = fields.Char(required=True, translate=True)
+
+    def action_book_list(self):
+        self.ensure_one()
+        return {
+            'name': _('Author Books'),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'tree,form',
+            'res_model': 'library.book',
+            'context': {'default_author_id': self.id},
+            'domain': [('author_id', '=', self.id)],
+        }
